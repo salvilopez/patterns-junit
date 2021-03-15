ec09c72c2b229cb5f2cf552ceda9a6418b9dc0d1
Salvador Sierra <salvilopezronda@gmail.com>
2021-03-15 20:46:30 +0100

    add @overrride in Circle.java

--- src/main/java/com/patterns/creational/prototype/Circle.java
+++ src/main/java/com/patterns/creational/prototype/Circle.java
@@ -24,6 +24,7 @@ public class Circle extends Shape{
         return Math.PI*radio*radio;
     }
 
+    
     public Circle clone() throws CloneNotSupportedException {
         return (Circle) super.clone();
     }
--- src/main/java/config/Log4jPropertiesConfiguration.java
+++ src/main/java/config/Log4jPropertiesConfiguration.java
@@ -4,7 +4,7 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
 public class Log4jPropertiesConfiguration{
-     public static Logger logger = Logger.getLogger(Log4jPropertiesConfiguration.class);
+     public static final Logger logger = Logger.getLogger(Log4jPropertiesConfiguration.class);
     public static void main(String[] args)
     {
         //PropertiesConfigurator is used to configure logger from properties file
